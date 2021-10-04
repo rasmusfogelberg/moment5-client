032e40ae2fbcb55871f627b32d9379de731c7857
Rasmus Fogelberg <fogelbergrasmus@gmail.com>
2021-10-04 18:11:20 +0200

    Make code prettier. Update delete function

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,17 @@
-"use strict";
-
 // Variables
-let courseListEl = document.getElementById("course-list");
-let addCoursebtn = document.getElementById("add-Course");
-let codeInput = document.getElementById("course-code");
-let nameInput = document.getElementById("course-name");
-let progressionInput = document.getElementById("course-progression");
-let syllabusInput = document.getElementById("course-syllabus");
+let courseListEl = document.querySelector("#course-list");
+let addCoursebtn = document.querySelector("#add-course");
+let deleteCourseButtons;
+
+const apiBaseURL = `http://api.fogelcode.com/moment5/api`;
+const courseForm = document.querySelector("#course-form");
 
 // Functions
-let getCourses = () => {
+const getCourses = () => {
   courseListEl.innerHTML = "";
+  deleteCourseButtons = document.querySelectorAll(".delete-course");
 
-  fetch("http://api.fogelcode.com/moment5/api/read.php")
+  fetch(`${apiBaseURL}/read.php`)
     .then((response) => response.json())
     .then((data) => {
       data.forEach((course) => {
@@ -21,42 +20,31 @@ let getCourses = () => {
         <li><strong>Name:</strong> ${course.name}</li>
         <li><strong>Progression:</strong> ${course.progression}</li>
         <li><strong><a href="${course.course_syllabus}" title="${course.course_syllabus}">Course Syllabus</a></strong></li>
-        <button id="${course.id}" onClick="deleteCourse(${course.id})">Delete</button>
-        <br>
+        <button class="delete-course" data-id="${course.id}">Delete</button>
         `;
       });
     });
+  // TODO: Awlays handle error!
 };
 
-let deleteCourse = (id) => {
-  fetch(`http://api.fogelcode.com/moment5/api/delete.php?id=${id}`, {
-    method: "DELETE",
+const deleteCourse = (id) => {
+  console.log("HELLO", id);
+  fetch(`${apiBaseURL}/delete.php?id=${id}`, {
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
   })
     .then((response) => response.json())
-    .then((data) => {
-      getCourses();
-    })
     .catch((error) => {
       console.log(`Error: ${error}`);
-    });
+    })
+    .finally(getCourses());
 };
 
-let addCourse = () => {
-  let code = codeInput.value;
-  let name = nameInput.value;
-  let progression = progressionInput.value;
-  let syllabus = syllabusInput.value;
-
-  let course = {
-    code: code,
-    name: name,
-    progression: progression,
-    syllabus: syllabus,
-  };
-
-  fetch(`http://api.fogelcode.com/moment5/api/create.php`, {
+const addCourse = (course) => {
+  fetch(`${apiBaseURL}/create.php`, {
     method: "POST",
-    body: json.stringify(course),
+    body: JSON.stringify(course),
   })
     .then((response) => response.json())
     .then((data) => {
@@ -75,4 +63,44 @@ let addCourse = () => {
 
 // Event listeners
 window.addEventListener("load", getCourses);
-addCoursebtn.addEventListener("click", addCourse);
+
+// Prevent reload when submitting the form
+courseForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // Construct a FormData object based on the inputs that live inside the form
+  new FormData(courseForm);
+});
+
+//
+courseForm.addEventListener("formdata", (event) => {
+  const data = event.formData; // FormData-> .get()
+  const course = {};
+
+  /**
+   * data.keys() will contain an array
+   * of all the keys inside the formData.
+   * This will get their values from the
+   * fields that live inside the form.
+   **/
+  for (let key of data.keys()) {
+    // Example of why this is needed:
+    // Name attribute is "course-code" we need to
+    // remove "course-", as API only accepts "code"
+    let keyWithoutPrefix = key.replace("course-", "");
+
+    if (keyWithoutPrefix === "syllabus") {
+      keyWithoutPrefix = `course_syllabus`;
+    }
+
+    course[keyWithoutPrefix] = data.get(key);
+  }
+
+  addCourse(course);
+});
+
+document.body.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-course")) {
+    console.log("Clicked a delete button!", event.target);
+    deleteCourse(event.target.dataset.id);
+  }
+});
